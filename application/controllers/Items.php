ab09b5940a60b52140223188dfd4b0ac2d625248
JonahBenares <jonahbenares.cenpri@gmail.com>
2020-06-11 11:18:31 +0800

    record activity logs when item there is item update and delete

--- application/controllers/Items.php
+++ application/controllers/Items.php
@@ -54,6 +54,18 @@ class Items extends CI_Controller {
         }
     }
 
+    public function activity_log($activity){
+        $timestamp = date('Y-m-d H:i:s');
+        $data = array(
+            "activity_time"=>$timestamp,
+            "activity_name"=>$activity,
+            "user_id"=>$_SESSION['user_id']
+        );
+
+        $this->super_model->insert_into("activity_logs", $data);
+    }
+
+
     public function index(){
         $this->load->view('template/header');
         $this->load->view('template/sidebar',$this->dropdown);
@@ -915,6 +927,9 @@ class Items extends CI_Controller {
                     'selling_price' => $this->input->post('selling'),
              );
 
+                $act = "Updated item details of item_id ". $item_id;
+                $this->activity_log($act);
+
               if($this->super_model->update_where("items", $data, "item_id", $item_id)){
                 echo $item_id;
               }
@@ -944,6 +959,9 @@ class Items extends CI_Controller {
     public function delete_item(){
         $id=$this->uri->segment(3);
         $this->load->model('super_model');
+        $act = 'Deleted item id '. $id;
+        $this->activity_log($act);
+        
         if($this->super_model->delete_data($id)){
             echo "<script>alert('Succesfully Deleted'); 
                 window.location ='".base_url()."index.php/items/item_list'; </script>";
