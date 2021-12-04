5ca87023728945c97151c35eff711b87aa16858c
unknown <glennpaul.cenpri@gmail.com>
2021-12-04 14:05:57 -0800

    enable grace to update price in receiving

--- application/views/receive/view_receive.php
+++ application/views/receive/view_receive.php
@@ -209,7 +209,7 @@
 											<td><?php echo $it['remarks']; ?></td>
 											<?php if($saved==0){ ?> 
 											<td><a onclick="update_prcmrk('<?php echo $it['riid'];?>','<?php echo base_url();?>')" title="Update Price & Remarks" class="btn btn-info "><span class="fa fa-pencil"></span></a></td>
-											<?php } else if($_SESSION['user_id']==5 && $saved==1) { ?>
+											<?php } else if($_SESSION['user_id']==5 OR $_SESSION['user_id']==13 && $saved==1) { ?>
 											<td><a onclick="update_prcmrk('<?php echo $it['riid'];?>','<?php echo base_url();?>')" title="Update Price & Remarks" class="btn btn-info "><span class="fa fa-pencil"></span></a></td>
 											<?php } else { ?>
 											<td><a onclick="update_prcmrk('<?php echo $it['riid'];?>','<?php echo base_url();?>')" title="Update Price & Remarks" class="btn btn-info " style ="pointer-events: none" disabled><span class="fa fa-pencil"></span></a></td>
