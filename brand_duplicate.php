ae03e8550c815449aec4bf730357b127ab2a0410
jbenares <jonahbenares@gmail.com>
2022-02-18 14:50:27 +0800

    brand correction

--- brand_duplicate.php
+++ brand_duplicate.php
@@ -11,11 +11,11 @@ while($row = $brands->fetch_assoc()){
 
 $select_brand = $con_brand->query("SELECT * FROM brand_2");
 while($row_select = $select_brand->fetch_assoc()){
-
-	$get_id = $con_brand->query("SELECT * FROM brand WHERE brand_name = '$row_select[brand_name]'");
+	$bname = trim($row_select['brand_name']);
+	$get_id = $con_brand->query("SELECT * FROM brand WHERE brand_name = '$bname'");
 	while($row_id = $get_id->fetch_assoc()){
 
-		if($row_select['brand_name'] == $row_id['brand_name']){
+		if(trim($row_select['brand_name']) == trim($row_id['brand_name'])){
 		//echo $row_select['brand_id'] . " - " .$row_select['brand_name'] . " - " . $row_id['brand_id']. "<br>";
 
 
@@ -32,8 +32,24 @@ while($row_select = $select_brand->fetch_assoc()){
 
         $update_restock =$con_brand->query("UPDATE restock_details SET brand_id='$row_select[brand_id]' WHERE brand_id='$row_id[brand_id]'");
 
+         $update_supplier_items =$con_brand->query("UPDATE supplier_items SET brand_id='$row_select[brand_id]' WHERE brand_id='$row_id[brand_id]'");
+
+         //echo "UPDATE supplier_items_2 SET brand_id='$row_select[brand_id]' WHERE brand_id='$row_id[brand_id]'<br>";
 		}
 
 
+	}
+}
+
+$select_supplier_items = $con_brand->query("SELECT brand_id FROM supplier_items");
+while($row_items = $select_supplier_items->fetch_assoc()){
+
+	$select_b =  $con_brand->query("SELECT brand_id FROM brand WHERE brand_id = '$row_items[brand_id]'");
+	$rows_brand = $select_b->num_rows;
+	$fetch =  $select_b->fetch_assoc();
+
+	if($row_items['brand_id'] !=0 && $rows_brand==0){
+
+		  $update_supplier_items =$con_brand->query("UPDATE supplier_items SET brand_id='0' WHERE brand_id='$fetch[brand_id]'");
 	}
 }
